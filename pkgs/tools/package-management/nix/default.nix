e0f5f4c0daeb585f2dc21c6444a11a6cde4e2345
Cassandra Comar <cassandra@ditto.live>
2022-02-21 00:02:15 -0500

    update nixUnstable for #6036

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -1,29 +1,22 @@
-{ lib
-, aws-sdk-cpp
-, boehmgc
-, callPackage
-, fetchFromGitHub
-, fetchurl
-, fetchpatch
+{ lib, aws-sdk-cpp, boehmgc, callPackage, fetchFromGitHub, fetchurl, fetchpatch
 , Security
 
-, storeDir ? "/nix/store"
-, stateDir ? "/nix/var"
-, confDir ? "/etc"
-}:
+, storeDir ? "/nix/store", stateDir ? "/nix/var", confDir ? "/etc" }:
 let
   boehmgc-nix_2_3 = boehmgc.override { enableLargeConfig = true; };
 
   boehmgc-nix = boehmgc-nix_2_3.overrideAttrs (drv: {
     # Part of the GC solution in https://github.com/NixOS/nix/pull/4944
-    patches = (drv.patches or [ ]) ++ [ ./patches/boehmgc-coroutine-sp-fallback.patch ];
+    patches = (drv.patches or [ ])
+      ++ [ ./patches/boehmgc-coroutine-sp-fallback.patch ];
   });
 
   aws-sdk-cpp-nix = (aws-sdk-cpp.override {
     apis = [ "s3" "transfer" ];
     customMemoryManagement = false;
   }).overrideDerivation (args: {
-    patches = (args.patches or [ ]) ++ [ ./patches/aws-sdk-cpp-TransferManager-ContentEncoding.patch ];
+    patches = (args.patches or [ ])
+      ++ [ ./patches/aws-sdk-cpp-TransferManager-ContentEncoding.patch ];
 
     # only a stripped down version is build which takes a lot less resources to build
     requiredSystemFeatures = null;
@@ -31,17 +24,17 @@ let
 
   common = args:
     callPackage
-      (import ./common.nix ({ inherit lib fetchFromGitHub; } // args))
-      {
-        inherit Security storeDir stateDir confDir;
-        boehmgc = boehmgc-nix;
-        aws-sdk-cpp = aws-sdk-cpp-nix;
-      };
+    (import ./common.nix ({ inherit lib fetchFromGitHub; } // args)) {
+      inherit Security storeDir stateDir confDir;
+      boehmgc = boehmgc-nix;
+      aws-sdk-cpp = aws-sdk-cpp-nix;
+    };
 in lib.makeExtensible (self: {
   nix_2_3 = (common rec {
     version = "2.3.16";
     src = fetchurl {
-      url = "https://nixos.org/releases/nix/nix-${version}/nix-${version}.tar.xz";
+      url =
+        "https://nixos.org/releases/nix/nix-${version}/nix-${version}.tar.xz";
       sha256 = "sha256-fuaBtp8FtSVJLSAsO+3Nne4ZYLuBj2JpD2xEk7fCqrw=";
     };
   }).override { boehmgc = boehmgc-nix_2_3; };
