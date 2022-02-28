24e57f7b4860732f37f988a1fc213e7b759ebf0b
Cassandra Comar <cassandra@ditto.live>
2022-02-28 12:26:43 -0500

    update to rust-analyzer 2022-02-28

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage rec {
     owner = "rust-analyzer";
     repo = "rust-analyzer";
     rev = version;
-    sha256 = "sha256-O9onJrAJY0TWxEZZcTWuF7SGPypRtSo0ZbpzDLC5+os=";
+    sha256 = "sha256-GQ1cPO4povnozLl0MTFs0ZXpmBn+AZeFWQVnxHHWf9g=";
   };
 
   patches = [
