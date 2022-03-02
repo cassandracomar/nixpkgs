c48639653b727592417d38143c1fbc1aa9c598e8
Cassandra Comar <cassandra@ditto.live>
2022-03-02 10:16:38 -0500

    updated firefox-nightly-bin

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -2,9 +2,9 @@
   version = "99.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2022/02/2022-02-21-09-40-19-mozilla-central/firefox-99.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2022/02/2022-02-28-21-45-38-mozilla-central/firefox-99.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-qiMr8mE6s2HXj1/h70iZ12j6dS04dQRx1XqJUf5wEuQ=";
+    sha256 = "sha256-NO15iImrEK2RUiEz1sQ9w0tCv4VigaI6nt8IR7IEhRQ=";
   }];
 }
