d5e71bc12aaf55b29a73214b2729b33ce11fe494
Cassandra Comar <cassandra@ditto.live>
2022-06-07 16:27:23 -0400

    fix sha

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -5,6 +5,6 @@
       "https://archive.mozilla.org/pub/firefox/nightly/2022/06/2022-06-07-09-34-40-mozilla-central/firefox-103.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-Hlw+B8EtzFkwf/JBYCygBljrN0viMw+kjLIpkhZahUk=";
   }];
 }
