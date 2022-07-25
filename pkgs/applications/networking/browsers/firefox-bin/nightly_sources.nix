fc093b1f6f911da96b92ff4b730fe105f48bdfd1
Cassandra Comar <cassandra@ditto.live>
2022-07-25 10:09:03 -0400

    update firefox and fix dup package

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -2,9 +2,9 @@
   version = "99.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2022/07/2022-07-13-09-38-28-mozilla-central/firefox-104.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2022/07/2022-07-25-09-49-20-mozilla-central/firefox-104.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-v/Twyh23SZm/kF1DHj7XXd/lREor/KIkupKPnnm4eiY=";
+    sha256 = "sha256-5q4iCnwY2rHKLboXWpg87mP5wF4PgQhZW3sHyQTo7GM=";
   }];
 }
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23863,8 +23863,6 @@ with pkgs;
   linux_xanmod_tt = linuxKernel.kernels.linux_xanmod_tt;
   linuxPackages_xanmod_latest = linuxKernel.packages.linux_xanmod_latest;
   linux_xanmod_latest = linuxKernel.kernels.linux_xanmod_latest;
-  linuxPackages_xanmod_tt = linuxKernel.packages.linux_xanmod_tt;
-  linux_xanmod_tt = linuxKernel.kernels.linux_xanmod_tt;
 
   linux-doc = callPackage ../os-specific/linux/kernel/htmldocs.nix { };
 
