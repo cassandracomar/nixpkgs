a869e8b5ad89f35d46371edc4c23986cfe307e2a
Cassandra Comar <cassandra@ditto.live>
2022-08-10 12:05:46 -0400

    update firefox nightly

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -1,10 +1,10 @@
 {
-  version = "99.0a1";
+  version = "105.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2022/07/2022-07-25-09-49-20-mozilla-central/firefox-104.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2022/08/2022-08-10-09-45-30-mozilla-central/firefox-105.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-5q4iCnwY2rHKLboXWpg87mP5wF4PgQhZW3sHyQTo7GM=";
+    sha256 = "sha256-6LperIPHEtrmatV7ZpFd/Ax4DDfMjYybBfReg3jejDQ=";
   }];
 }
