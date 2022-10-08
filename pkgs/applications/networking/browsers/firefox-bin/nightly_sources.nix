0355baa0d59e63046f52ca99277b3a2f63713247
Cassandra Comar <cassandra@ditto.live>
2022-10-08 04:02:25 -0400

    update firefox

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -2,9 +2,9 @@
   version = "105.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2022/08/2022-08-10-09-45-30-mozilla-central/firefox-105.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2022/10/2022-10-07-21-38-10-mozilla-central/firefox-107.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-6LperIPHEtrmatV7ZpFd/Ax4DDfMjYybBfReg3jejDQ=";
+    sha256 = "sha256-ZNoW2c2hhe1GIvxotiQbP38wmK8xdoVoFK0QbRI30aQ=";
   }];
 }
