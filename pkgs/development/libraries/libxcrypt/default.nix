3de2b8900cbce4691c759f98e9edd0fe6d252bac
Cassandra Comar <cassandra@ditto.live>
2022-11-03 13:29:25 -0400

    disable werror for libxcrypt

--- pkgs/development/libraries/libxcrypt/default.nix
+++ pkgs/development/libraries/libxcrypt/default.nix
@@ -18,7 +18,6 @@ stdenv.mkDerivation rec {
     "--enable-hashes=all"
     "--enable-obsolete-api=glibc"
     "--disable-failure-tokens"
-  ] ++ lib.optionals stdenv.hostPlatform.isMusl [
     "--disable-werror"
   ];
 
