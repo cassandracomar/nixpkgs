f55f5c3114421f416d4efed52ecd0a9682c91cc0
Cassandra Comar <cassandra@ditto.live>
2022-12-19 15:41:27 -0500

    don't run libffi tests

--- pkgs/development/libraries/libffi/default.nix
+++ pkgs/development/libraries/libffi/default.nix
@@ -3,7 +3,7 @@
 
   # test suite depends on dejagnu which cannot be used during bootstrapping
   # dejagnu also requires tcl which can't be built statically at the moment
-, doCheck ? !(stdenv.hostPlatform.isStatic)
+, doCheck ? false
 , dejagnu
 , nix-update-script
 }:
