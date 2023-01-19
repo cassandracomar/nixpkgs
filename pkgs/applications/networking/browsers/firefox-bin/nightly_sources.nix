61c5b0edeba0e20ccedcfe233dfe709d29f3dc9b
Cassandra Comar <cassandra@ditto.live>
2023-01-19 10:40:06 -0500

    update to firefox nightly 01-19-2023

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -1,10 +1,10 @@
 {
-  version = "105.0a1";
+  version = "111.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2022/12/2022-12-19-09-38-06-mozilla-central/firefox-110.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2023/01/2023-01-19-09-31-27-mozilla-central/firefox-111.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-DAo6nhwFo2SShp1CijYiJIn0zMQzmGJbKT35CbOh2ZQ=";
+    sha256 = "sha256-ONQ9fWIQGrzjhjXXU1noMPyu3KdYC1J0PSBQULNtijg=";
   }];
 }
