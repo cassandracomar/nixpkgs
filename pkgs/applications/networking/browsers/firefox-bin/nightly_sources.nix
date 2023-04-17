f3820dcf20f594268c47a906538534bee16e4f50
Cassandra Comar <cassandra@ditto.live>
2023-04-17 11:03:10 -0400

    update firefox-nightly-bin to 04-17-2023

--- pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
+++ pkgs/applications/networking/browsers/firefox-bin/nightly_sources.nix
@@ -1,10 +1,10 @@
 {
-  version = "113.0a1";
+  version = "114.0a1";
   sources = [{
     url =
-      "https://archive.mozilla.org/pub/firefox/nightly/2023/03/2023-03-21-09-19-11-mozilla-central/firefox-113.0a1.en-US.linux-x86_64.tar.bz2";
+      "https://archive.mozilla.org/pub/firefox/nightly/2023/04/2023-04-17-09-21-44-mozilla-central/firefox-114.0a1.en-US.linux-x86_64.tar.bz2";
     locale = "en-US";
     arch = "linux-x86_64";
-    sha256 = "sha256-4iWIWHZUXDyDt3cKqVwJpQ5dpFRAZg75HwXt8VMRhE8=";
+    sha256 = "sha256-yf3tGlzuX6mLwQ1TaxC1Vr2A4TjC2wyE33ZhioxEzIo=";
   }];
 }
