2623ec637707a8ae0d6cc729c8b1f7d202aa89bc
Cassandra Comar <cass@ndra.io>
2023-05-04 10:37:18 -0400

    revert removing zfs patches

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -78,11 +78,11 @@ let
       };
 
       patches = [
-        # (fetchpatch {
-        #   name = "musl.patch";
-        #   url = "https://github.com/openzfs/zfs/commit/1f19826c9ac85835cbde61a7439d9d1fefe43a4a.patch";
-        #   sha256 = "XEaK227ubfOwlB2s851UvZ6xp/QOtYUWYsKTkEHzmo0=";
-        # })
+        (fetchpatch {
+          name = "musl.patch";
+          url = "https://github.com/openzfs/zfs/commit/1f19826c9ac85835cbde61a7439d9d1fefe43a4a.patch";
+          sha256 = "XEaK227ubfOwlB2s851UvZ6xp/QOtYUWYsKTkEHzmo0=";
+        })
       ] ++ extraPatches;
 
       postPatch = optionalString buildKernel ''
